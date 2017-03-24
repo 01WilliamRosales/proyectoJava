ec4672ba60579421521844e4581ab43843d8096f
adminNIES <adminNIES@192.168.8.103>
2017-03-23 20:42:05 -0600

    modificado por nicolas

---
+++ src/proyectojava/ProyectoJava.java
@@ -0,0 +1,21 @@
+package proyectojava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author adminNIES
+ */
+public class ProyectoJava {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        String nombre;
+        System.out.println("BIENVENIDO");
+        System.out.println("ESTA ES UNA PRUEBA DE USO DE GITHUB");
+        System.out.println("ingresa tu nombre");
+        nombre = leer.next();
+        System.out.println("Gracias por realizar esta prueba " + nombre + " NOS ESPERA UN LARGO CAMINO");
+    }
+    
+}
